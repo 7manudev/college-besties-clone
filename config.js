f0b3a511baf13d4df822ba17ebfb62cb8d3f90e9
7devmanu <smanuelpadovani@gmail.com>
2026-06-24 03:24:38 +0300

    Replace fake profiles with Milli Moon

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-import { portraitFor } from "./utils.js";
-
 export const SITE = {
   name: "YourGirls",
   tagline: "Discover your",
@@ -10,30 +8,13 @@ export const SITE = {
 };
 
 export const FEATURED = {
-  name: "Blair",
-  img: portraitFor("Blair"),
+  name: "Milli Moon",
+  img: "mili.jpg",
   bio: "just an 18 y/o who's not mean to be on here — come say hi!!",
   isNew: true,
-  profileUrl: "#configure-me",
+  profileUrl: "https://onlyfans.com/millimoonfree/c1",
 };
 
 export const CREATORS = [
-  { name: "Addison B", img: portraitFor("Addison B"), niche: "white", hair: "blonde", vibe: "preppy", isNew: false, profileUrl: "#" },
-  { name: "Amber", img: portraitFor("Amber"), niche: "white", hair: "brunette", vibe: "preppy", isNew: true, profileUrl: "#" },
-  { name: "Autumn", img: portraitFor("Autumn"), niche: "white", hair: "blonde", vibe: "girl-next-door", isNew: false, profileUrl: "#" },
-  { name: "Blair", img: portraitFor("Blair"), niche: "white", hair: "blonde", vibe: "girl-next-door", isNew: false, profileUrl: "#" },
-  { name: "Cora", img: portraitFor("Cora"), niche: "white", hair: "brunette", vibe: "glam", isNew: false, profileUrl: "#" },
-  { name: "Estelle", img: portraitFor("Estelle"), niche: "white", hair: "brunette", vibe: "glam", isNew: false, profileUrl: "#" },
-  { name: "Hartley", img: portraitFor("Hartley"), niche: "", hair: "blonde", vibe: "girl-next-door", isNew: true, profileUrl: "#" },
-  { name: "Jordin", img: portraitFor("Jordin"), niche: "white", hair: "brunette", vibe: "girl-next-door", isNew: false, profileUrl: "#" },
-  { name: "Jovie", img: portraitFor("Jovie"), niche: "egirl", hair: "brunette", vibe: "sporty", isNew: false, profileUrl: "#" },
-  { name: "Kimmy", img: portraitFor("Kimmy"), niche: "", hair: "brunette", vibe: "girl-next-door", isNew: true, profileUrl: "#" },
-  { name: "Kelsey", img: portraitFor("Kelsey"), niche: "white", hair: "blonde", vibe: "girl-next-door", isNew: true, profileUrl: "#" },
-  { name: "Loren", img: portraitFor("Loren"), niche: "white", hair: "brunette", vibe: "preppy", isNew: false, profileUrl: "#" },
-  { name: "Miley", img: portraitFor("Miley"), niche: "mixed", hair: "brunette", vibe: "preppy", isNew: false, profileUrl: "#" },
-  { name: "Piper", img: portraitFor("Piper"), niche: "white", hair: "brunette", vibe: "girl-next-door", isNew: false, profileUrl: "#" },
-  { name: "Quinn", img: portraitFor("Quinn"), niche: "white", hair: "blonde", vibe: "sporty", isNew: false, profileUrl: "#" },
-  { name: "Reagan", img: portraitFor("Reagan"), niche: "latina", hair: "blonde", vibe: "preppy", isNew: false, profileUrl: "#" },
-  { name: "Stephanie", img: portraitFor("Stephanie"), niche: "white", hair: "blonde", vibe: "preppy", isNew: false, profileUrl: "#" },
-  { name: "Gia", img: portraitFor("Gia"), niche: "", hair: "brunette", vibe: "glam", isNew: true, profileUrl: "#" },
+  { name: "Milli Moon", img: "mili.jpg", niche: "cosplay", hair: "blonde", vibe: "girl-next-door", isNew: true, profileUrl: "https://onlyfans.com/millimoonfree/c1" },
 ];
